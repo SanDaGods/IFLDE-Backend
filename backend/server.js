38de49016be874f46825498898695a3ab2e1a47f
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-18 14:50:20 +0800

    8080

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // ======================
 // Environment Variables
 // ======================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const PROD_FRONTEND_URL = "https://ifldefrontend-production.up.railway.app";
 const ALLOWED_ORIGINS = [
   PROD_FRONTEND_URL,
